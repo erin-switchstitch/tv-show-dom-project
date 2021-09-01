93e0650f46db4bc5d0e412e18a4191e2e810c632
erin-switchstitch <83361880+erin-switchstitch@users.noreply.github.com>
2021-09-01 14:43:52 +0100

    V.1.2 - script.js update
    Modified script.js to import all episodes onto the page

--- script.js
+++ script.js
@@ -20,49 +20,42 @@ function popUpFunctionIdSpecific() {
   popup.classList.toggle("show");
 }
 
-
 const flexOuterContainer = document.querySelector(".flexOuterContainer");
-cl(flexOuterContainer)
+cl(flexOuterContainer);
 const allEpisodes = getAllEpisodes();
-cl(allEpisodes.length)
+cl(allEpisodes.length);
 
 for (let index = 0; index < allEpisodes.length; index++) {
   const currentEpisode = allEpisodes[index];
-  cl(currentEpisode)
+  cl(currentEpisode);
   //cl(flexOuterContainer.innerHTML)
   flexOuterContainer.innerHTML += `
    <div class="flexEpisodeContainer">
         <h2 class="episodeTitle">${currentEpisode.name}</h2>
-        <h3 class="episodeNumberElement">Season<span class="seasonNum">1 </span> Episode <span
-            class="episodeNum">1</span></h3>
+        <h3 class="episodeNumberElement">Season<span class="seasonNum"> ${currentEpisode.season}</span> Episode <span
+            class="episodeNum"> ${currentEpisode.number}</span></h3>
 
         <div class="episodeImageContainer">
-          <img class="episodeImage" src="http://static.tvmaze.com/uploads/images/medium_landscape/1/2668.jpg"
-            alt="placeholder image">
+          <img class="episodeImage" src=${currentEpisode.image.medium}
+            alt="Season ${currentEpisode.season}, Episode ${currentEpisode.number} Image">
           <div class="imageOverlayTextContainer">
             <div class="imageOverlayText">
-              <h4>Air Date : <span>17-04-2011</span></h4>
-              <h4>Air Time : <span>21.00</span></h4>
-              <h4>Runtime : <span>60</span> mins</h4>
+              <h4>Air Date : <span>${currentEpisode.airdate}</span></h4>
+              <h4>Air Time : <span>${currentEpisode.airtime}</span></h4>
+              <h4>Runtime : <span>${currentEpisode.runtime}</span> mins</h4>
             </div>
           </div>
         </div>
 
-        <p class="episodeSummary">Lord Eddard Stark, ruler of the North, is summoned to court by his old friend, King
-          Robert Baratheon, to serve as the
-          King's Hand. Eddard reluctantly agrees after learning of a possible threat to the King's life. Eddard's
-          bastard son Jon
-          Snow must make a painful decision about his own future, while in the distant east Viserys Targaryen plots to
-          reclaim his
-          father's throne, usurped by Robert, by selling his sister in marriage.</p>
+        <p class="episodeSummary">${currentEpisode.summary}</p>
 
         <button class="popupButton" onclick="popUpFunctionIdSpecific()">See More Info
           <span id="PLACEHOLDERmyPopup" class="popupButtonText" id="myPopup">
-            <a class="tvMazeLink" href="https://www.w3schools.com/" target="_blank">TV Maze Episode Link</a>
+            <a class="tvMazeLink" href=${currentEpisode._links.self.href} target="_blank">TV Maze Episode Link</a>
             <a class="imbdLink" href="https://www.w3schools.com/" target="_blank">IMBD Episode Link</a>
           </span>
         </button>
 
       </div>
-  `
+  `;
 }
